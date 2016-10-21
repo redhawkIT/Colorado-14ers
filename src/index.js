65fd30c7b2857b951357e585812560251512c66f
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-21 07:28:36 -0500

    add redux  and material ui boilercode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import './index.css'
+import myTheme from './theme'
+
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducer from './reducers/index'
+let store = createStore(reducer)
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+const Main = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(myTheme)}>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </MuiThemeProvider>
+)
+
+render(
+  <Main/>, document.getElementById('root'))
