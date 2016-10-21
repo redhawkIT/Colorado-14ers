acc11234a59ccbe91cb982536169db398675291a
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-21 12:11:54 -0500

    add containers

---
+++ src/containers/VisibleCards.js
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux'
+import CardList from '../components/CardList'
+
+import getVisibleMountains from '../helpers/getVisibleMountains'
+import mountains from '../data/mountains'
+
+const mapStateToProps = (state) => {
+  return {
+    mountains: getVisibleMountains(
+      mountains,
+      state.search.query,
+      state.search.filters
+    )
+  }
+}
+
+const VisibleCards = connect(mapStateToProps)(CardList)
+
+export default VisibleCards
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import App from './components/App'
+import App from './App'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
